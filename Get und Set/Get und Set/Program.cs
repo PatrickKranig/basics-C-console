66f1e85762e134c74f6842a191f0c3a5b8fcaa3c
Patrick Kranig <Patrick.Kranig@live.de>
2019-01-21 11:15:42 +0100

    Add files via upload

---
+++ Get und Set/Get und Set/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_und_Set
+{
+    //https://www.youtube.com/watch?v=qXj4B4l92_A
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm Start = MAIN Methode");
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Klasse Hendrik vom Typ Person deklarieren (intanziieren)");
+            Person hendrik = new Person();
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("hendrik den wert 10 zuweisen (set):");
+            hendrik.Alter = 10; //set methode, zum setzen
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Jetzt kommt die Ausgabe von hendrik.Alter (get- holen):");
+            Console.WriteLine(hendrik.Alter ); //get methode, zum holen
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("sinn? wir können nicht auf die private Variable der Klasse Person zugreifen, nur eben über die Eigenschaft x.Alter -> sehr sauberer und sicherer Porgammierstil");
+            Console.ReadKey();
+
+            /*Der Unterschied liegt in der Datenkapselung begründet. Ich habe dazu mal etwas vorbereitet ;-) https://www.lernmoment.de/alle/was-ist-datenkapselung/ 
+             * Wenn du eine Eigenschaft verwendest hast du später noch die Chance beim Setzen oder Auslesen des Wertes gewisse Bedingungen zu prüfen o.ä. Grundsätzlich
+             * empfehle ich immer ein Attribut NICHT public zu machen! Der Kerngedanke von OO ist, dass du Methoden in einer Klasse hast die die Attribute verändern. 
+             * Lässt du die Attribute von ausserhalb der Klasse verändern, dass weisst du einerseits nicht was alles reinkommt und andererseits hast du deine Datenkapselung
+             * wesentlich verwässert. Es geht schließlich immer darum, dass du eine Verantwortung in deiner Klasse umsetzt und niemand ausserhalb deiner Klasse wissen muss
+             * wie du das umgesetzt hast. Eine andere Erklärung: http://www.inf-schule.de/modellierung/ooppython/roboter/datenkapselung/konzept_datenkapselung */
+
+        }
+    }
+    class Person
+    {
+        //Private Variablen
+        private int PrivateVariable;
+
+        //Eigenschaften (händisch get und set)
+        public int Alter //das get, set schreiben wir jetzt mal verdeutlicht ausführlich rein, eigentlich reicht nur:
+                         //public int Alter {get; set;}
+                         //<- wenn wir das so machen, wird automatisch eine "unsichtbare" private variable "alter" für diese Klasse deklariert
+        {
+            get
+            {
+                Console.WriteLine("Ausgabe, dass die Get Methode ausgeführt wird: Alter wurde eingelesen"); //zur verdeutlichung
+                return PrivateVariable; //Gebe den Wert der Privaten Variable den wir nur in der Klasse benutzen dürfen als Eigeschaftswert für die Instanzierung (OOP) zurück
+                //Private Varialble ist noch 0, da wir nur "Getten" - holen/lesen
+            }
+            set
+            {
+                Console.WriteLine("Ausgabe, dass die Set Methode ausgeführt wird: Alter wird neu geschrieben");//zur verdeutlichung
+                PrivateVariable = value; //value (wert) ist das Schlüsselwort für den Zuweisungswert (variabel je nach Objekt/Code)
+            }//(wie rum, ist egal)
+        }
+    }
+}
---
+++ Klassen/Klassen/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen
+{
+    //https://www.youtube.com/watch?v=40hQ6FnqRSI&list=PL_pqkvxZ6ho18awjThtUMZio-yvc79TGi&index=14
+    //https://www.youtube.com/watch?v=puf8z--uDp8&index=15&list=PL_pqkvxZ6ho18awjThtUMZio-yvc79TGi Eigenschaften...
+    class Program //Wir haben eine neue Klasse erstellt, mit der wir arbeiten können
+    {
+        class Person //erstelle eine ganz neue Klassen (wie Console oder sonst was)
+        {
+            //Eigenschaften setzen - sie beschreiben eine Klasse/Objekt - das sind keine Variablen!!!
+            //- sie sind ein Konstrukt aus 2 Methoden (get und set) und einer privaten veriable
+            public string Name { get; set; } //public ist der zugriffsmodifizierer, zum festlegen dass man auf die Eigenschaft von außen zugreifen darf
+            //wenn wir public weglassen, wird es automatisch als private gewertet!
+            public int Alter { get; set; } //es gibt nur 4 zugriffsmodifizierer public, protected, internal, private
+                                           //Zugrfiffsmodifizierer: https://docs.microsoft.com/de-de/dotnet/csharp/language-reference/keywords/access-modifiers
+
+//was ist der unterschied zu get und set?
+/* Der Unterschied ist der, dass du eine private Variable mit einer Eigenschaft kapselst. Das bedeutet, 
+ * dass der Zugriff auf die private Variable über die Eigenschaft kontrolliert erfolgt. Du kannst im Getter 
+ * und im Setter beispielsweise Abfragen machen und den Zugriff an bestimmte Bedingungen knüpfen. So ist es zum 
+ * Beispiel möglich, dass du im Setter nur bei bestimmten Bedingungen wirklich eine Zuweisung auf die private Variable 
+ * ermöglichst. Bei einer einfachen Variable ist das nicht möglich! :)*/
+  //get damit wir aus der Klasse die Eigenschaft lesen können, set damit wir aus der Klasse die Eigenschaft lesen können
+
+  private bool tot2; //Diese Variable zählt nur für diese Klasse
+  public bool tot; //Diese Variable ost von außen ausserhalb der Klasse aufrufbar
+
+
+  //Methoden schreiben
+  public void SageHallo()
+  {
+      Console.WriteLine(Name + " sagt Hallo!");
+  }
+}
+
+  static void Main(string[] args) //Einsteigspunkt für das Programm
+  {
+     //Objekt vom Typ Person erstellen ( Person ist jetzt ein Datentyp)
+     Person janek = new Person(); //in janek steckt die Objektinstqanz von Person - Objekt instanzieieren
+
+     janek.Name = "Janek"; //Definition eines Objektes (set Methode)
+     janek.SageHallo(); //führe nun diese Methode der neuen Klassse aus - das ganze spart code und macht es übersichtlicher beim coden
+
+     Console.WriteLine("");
+     Console.WriteLine("jetzt habe ich ein ganz neues Objekt 'Hendrik' erstellt!"); //(mit neuen Eigenschaften)
+     Console.WriteLine("");
+
+     Person hendrik = new Person(); //Hendrik ist nur die Variable vom Typ (der Klasse) Person
+
+     hendrik.Name = "Hendrick"; //Hendrik ist ein ganz neues Objekt!
+     int alter = hendrik.Alter = 22; //lege die Initialisierung der Eigesnschaft auf eine Variable
+         //wenn wir das ganze über die get methode bei der ausgabe laufen lassen wollen, brauchen wir keine variable
+     Console.WriteLine("Hendrik ist {0} Jahre alt", alter);
+     //Console.WriteLine("Hendrik ist {0} Jahre alt", hendrik.Alter); //alternativ auch möglich: wir nuten die implementierte get methode
+     hendrik.tot = true; //Arbeiten mit KlassenVariablen - tot2 ist nicht initialisierbar, weil private
+     Console.WriteLine("ist hendrick tot? " + hendrik.tot + " (natürlich nicht, mir fiel nur schnell nix ein =P");
+     hendrik.SageHallo();
+
+     Console.ReadKey();
+
+  //das ganze ist Objektorientierte Programmierung (OOP)
+  //. ist der c# standard Operator für Eigenschaften
+  }
+
+
+}
+}
+
+
